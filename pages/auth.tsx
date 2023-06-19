a3814f96f4809dd2cf878f8cc0eed430aab005a5
Devi Amaolo <deviamaolo@gmail.com>
2023-06-18 21:00:35 -0300

    add auth background

---
+++ pages/auth.tsx
@@ -0,0 +1,18 @@
+const Auth = () => {
+	return (
+		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
+			<div className="bg-black w-full h-full lg:bg-opacity-50">
+				<nav className="px-12 py-5">
+					<h1 className="text-red-700 text-4xl font-bold">Freeflix</h1>
+					<div className="flex justify-center">
+						<div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg-max-w-md rounded-md w-full">
+							
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+	)
+}
+
+export default Auth
