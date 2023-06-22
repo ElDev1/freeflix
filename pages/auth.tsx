694a453e1f6411769346d97554784f77bad95719
Devi Amaolo <deviamaolo@gmail.com>
2023-06-21 23:11:27 -0300

    add state to input component

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,17 @@
+import { useState, useCallback } from 'react'
 import Input from '../components/Input'
 
 const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [variant, setVariant] = useState('login')
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === "login" ? "register": "login")
+  }, [])
+
 	return (
 		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
 			<div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -12,8 +23,36 @@ const Auth = () => {
 								Sign in
 							</h2>
 							<div className="flex flex-col gap-4">
-								<Input />
+                <Input 
+                  label="Username"
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  value={name}
+                />
+                <Input 
+                  label="Email"
+                  onChange={(e: any) => setEmail(e.target.value)}
+                  id="email"
+                  type="email"
+                  value={email}
+								/>
+                <Input 
+                  label="Password"
+                  onChange={(e: any) => setPassword(e.target.value)}
+                  id="password"
+                  type="password" 
+                  value={password}
+								/>
 							</div>
+              <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                Login
+              </button>
+              <p className="text-neutral-500 mt-12">
+                First time using Freeflix?
+                <span className="text-white ml-1 hover:underline cursor-pointer">
+                  Create an account
+                </span>
+              </p>
 						</div>
 					</div>
 				</nav>
