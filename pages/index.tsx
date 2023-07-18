53c908bb31b15fc2154b9077eb8eeea45c2178c8
Devi Amaolo <deviamaolo@gmail.com>
2023-07-18 01:26:23 -0300

    create profiles route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,32 @@
-import type { NextPage } from 'next'
+import type { NextPage, NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
+import useCurrentUser from '../hooks/useCurrentUser'
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 const Home: NextPage = () => {
+  const { data: user } = useCurrentUser()
+
   return (
     <div>
-      Freeflix
+      <h1 className='text-xl text-green-500'>Freeflix</h1>
+      <p className='text-white'>Logged in as: {user?.email}</p>
+      <button className='h-10 px-2 bg-white' onClick={() => signOut()}>logout</button>
     </div>
   )
 }
