7e0dfa2583f610612df0a697b168ae85ee5a4eed
Devi Amaolo <deviamaolo@gmail.com>
2023-07-22 23:50:58 -0300

    create mobileMenu component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
-import NavbarItem from "./NavbarItem"
+import { BsChevronDown } from 'react-icons/bs'
+import { useState, useCallback } from 'react'
+
+import NavbarItem from './NavbarItem'
+import MobileMenu from './MobileMenu'
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current)
+  }, [])
+
   return (
     <nav>
       <div className='px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90'>
@@ -12,12 +22,14 @@ const Navbar = () => {
           <NavbarItem label='My list'/>
           <NavbarItem label='Browse by languages'/>
         </div>
-        <div className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'>
+        <div onClick={toggleMobileMenu} className='lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative'>
           <p className="text-white text-sm">Browse</p>
+          <BsChevronDown className='text-white transition'/>
+          <MobileMenu visible={showMobileMenu} /> 
         </div>
       </div>
     </nav>
-  )
+  ) 
 }
 
 export default Navbar
