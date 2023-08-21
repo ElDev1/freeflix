cac69bac0da1d9f510a28dba681621a8de697584
Devi Amaolo <deviamaolo@gmail.com>
2023-08-21 20:33:36 -0300

    create movie route and useMovie hook

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useCallback, useMemo } from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlinePlus, AiOutlineCheck } from 'react-icons/ai'
 
 import useCurrentUser from '../hooks/useCurrentUser'
 import useFavorites from '../hooks/useFavorites'
@@ -9,40 +9,46 @@ interface FavoriteButtonProps {
   movieId: string
 }
 
+interface FavoriteButtonProps {
+  movieId: string
+}
+
 const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  const { mutate: mutateFavorites } = useFavorites()
-  const { data: currentUser, mutate } = useCurrentUser()
-  
+  const { mutate: mutateFavorites } = useFavorites();
+
+  const { data: currentUser, mutate } = useCurrentUser();
+
   const isFavorite = useMemo(() => {
-    const list = currentUser?.favoriteIds || []
+    const list = currentUser?.favoriteIds || [];
 
-    return list.includes(movieId)
-  }, [currentUser, movieId])
+    return list.includes(movieId);
+  }, [currentUser, movieId]);
 
-  const toogleFavorites = useCallback(async () => {
-    let response
-    
-    if(isFavorite) {
-      response = await axios.delete('/api/favorite', { data: { movieId }})
+  const toggleFavorites = useCallback(async () => {
+    let response;
+
+    if (isFavorite) {
+      response = await axios.delete('/api/favorite', { data: { movieId } });
     } else {
-      response = await axios.post('/api/favorite', { movieId })
+      response = await axios.post('/api/favorite', { movieId });
     }
 
-    const updatedFavoriteIds = response?.data?.favoritesIds
+    const updatedFavoriteIds = response?.data?.favoriteIds;
 
-    mutate({
-      ...currentUser,
-      favoriteIds: updatedFavoriteIds
-    })
-
-    mutateFavorites()
-  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites])
+    mutate({ 
+      ...currentUser, 
+      favoriteIds: updatedFavoriteIds,
+    });
+    mutateFavorites();
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+  
+  const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
   return (
-    <div className='cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center transition hover:border-netural-300'>
-      <AiOutlinePlus className='text-white' size={25} />
+    <div onClick={toggleFavorites} className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+      <Icon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
     </div>
   )
 }
 
-export default FavoriteButton
+export default FavoriteButton;
--- pages/api/favorite.ts
+++ pages/api/favorite.ts
@@ -7,20 +7,20 @@ import serverAuth from '../../lib/serverAuth'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method === 'POST') {
-      const { currentUser } = await serverAuth(req, res)
+      const { currentUser } = await serverAuth(req, res);
 
-      const { movieId } = req.body
-
+      const { movieId } = req.body;
+  
       const existingMovie = await prismadb.movie.findUnique({
         where: {
           id: movieId,
         }
-      })
-
-      if(!existingMovie) {
-        throw new Error('Invalid ID')
+      });
+  
+      if (!existingMovie) {
+        throw new Error('Invalid ID');
       }
-
+  
       const user = await prismadb.user.update({
         where: {
           email: currentUser.email || '',
@@ -30,43 +30,44 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             push: movieId
           }
         }
-      })
-
-      return res.status(200).json(user)
+      });
+  
+      return res.status(200).json(user);
     }
 
-    if(req.method === 'DELETE') {
-      const { currentUser } = await serverAuth(req, res)
+    if (req.method === 'DELETE') {
+      const { currentUser } = await serverAuth(req, res);
 
-      const { movieId } = req.body
+      const { movieId } = req.body;
 
       const existingMovie = await prismadb.movie.findUnique({
         where: {
           id: movieId,
         }
-      })
+      });
 
-      if(!existingMovie) {
-        throw new Error('invalid ID')
+      if (!existingMovie) {
+        throw new Error('Invalid ID');
       }
 
-      const updatedFavoritesIds = without(currentUser.favoriteIds, movieId)
+      const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
 
       const updatedUser = await prismadb.user.update({
         where: {
           email: currentUser.email || '',
         },
         data: {
-          favoriteIds: updatedFavoritesIds
+          favoriteIds: updatedFavoriteIds,
         }
-      })
+      });
 
-      return res.status(200).json(updatedUser)
+      return res.status(200).json(updatedUser);
     }
+    
+    return res.status(405).end();
+  } catch (error) {
+    console.log(error);
 
-    return res.status(405).end()
-  } catch (err) {
-    console.log(err)
-    return res.status(400).end()
+    return res.status(500).end();
   }
 }
